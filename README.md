17483b7856de947ebf7687a2b4f08eddae78e6a1
Kyle Chan <kylec725@gmail.com>
2021-01-13 15:42:59 -0500

    initial commit

--- README.md
+++ README.md
@@ -1,1 +1,13 @@
 # rtouch
+rtouch is a recursive touch utility designed to touch existing files, or create
+new files while making the required directories for the new file.
+
+rtouch is inspired by [Advanced New File](https://github.com/tanrax/terminal-AdvancedNewFile)
+
+## Usage
+```
+rtouch path1/file1 path2/file2 ... pathn/filen
+```
+
+## TODO
+- Add existing touch command flags
---
+++ main.go
@@ -0,0 +1,47 @@
+package main
+
+import (
+    "fmt"
+    "os"
+    "path/filepath"
+    "time"
+)
+
+func main() {
+    for _, arg := range os.Args[1:] {
+        err := touch(arg)
+        if err != nil {
+            fmt.Fprintln(os.Stderr, "gotouch:", err)
+        }
+    }
+}
+
+func touch(path string) error {
+    var err error
+    dir := filepath.Dir(path)
+
+    // Create directories recursively
+    if dir != "" {
+        err = os.MkdirAll(dir, 0755)
+        if err != nil {
+            return err
+        }
+    }
+
+    // Touch the file
+    if _, err = os.Stat(path); os.IsNotExist(err) {
+        file, err := os.Create(path)
+        if err != nil {
+            return err
+        }
+        file.Close()
+    } else {
+        now := time.Now().Local()
+        err = os.Chtimes(path, now, now)
+        if err != nil {
+            return err
+        }
+    }
+
+    return nil
+}
