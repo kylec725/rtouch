9fd8912928d91056ccd0d15ddbda0cf696506e3e
Kyle Chan <kylec725@gmail.com>
2021-01-13 18:17:01 -0500

    update comments

--- main.go
+++ main.go
@@ -28,7 +28,7 @@ func touch(path string) error {
         }
     }
 
-    // Touch the file
+    // Touch the file or directory
     if _, err = os.Stat(path); os.IsNotExist(err) {
         file, err := os.Create(path)
         if err != nil {
